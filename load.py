18b6490bff3e28810f2f967c7b87767e477b40fa
Rohit P Suresh <93090543+ElNino9495@users.noreply.github.com>
2024-04-16 15:38:43 +0530

    Add files via upload

---
+++ load.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from main import BigramLanguageModel, decode
+
+model = BigramLanguageModel()
+model.load_state_dict(torch.load('model.pth'))
+model.eval()
+# generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device='cpu')
+print(decode(model.generate(context, max_new_tokens=2000)[0].tolist()))
